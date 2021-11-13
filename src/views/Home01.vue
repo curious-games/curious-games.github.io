dace42dbc9094541e915e90ca2130bffe446e2c2
Tarik Kavaz <tarikkavaz@gmail.com>
2021-11-13 19:15:26 +0300

    preload test

--- src/views/Home01.vue
+++ src/views/Home01.vue
@@ -41,6 +41,21 @@
     components: {
       // Carousel,
     },
+    beforeCreate() {
+      function preloadImages(srcs) {
+          if (!preloadImages.cache) {
+              preloadImages.cache = [];
+          }
+          var img;
+          for (var i = 0; i < srcs.length; i++) {
+              img = new Image();
+              img.src = srcs[i];
+              preloadImages.cache.push(img);
+          }
+      }
+      var imageSrcs = ["/img/sequence/robodog-md/"];
+      preloadImages(imageSrcs);
+    },
     mounted() {
 
       const showSequenceImages = [];
