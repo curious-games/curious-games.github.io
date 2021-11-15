e47035853a451c95d9b1dcf3b8e3d13c30861119
Tarik Kavaz <tarikkavaz@gmail.com>
2021-11-15 16:26:26 +0300

    games bg

--- src/views/Games.vue
+++ src/views/Games.vue
@@ -2,25 +2,25 @@
   <div class="pt-28">
     <div class="relative h-80 md:h-160">
       <div class="absolute inset-0 z-0 bg-center bg-cover" style="background-image: url('/img/games/CockroachRacers.png')"></div>
-      <a href="#" class="absolute inset-0 z-10 flex items-center justify-center text-6xl font-semibold text-white duration-300 opacity-0 bg-cur-pink/10 hover:opacity-100 ">
+      <a href="#" class="absolute inset-0 z-10 flex items-center justify-center text-6xl font-semibold text-white duration-300 opacity-0 bg-black/50 hover:opacity-100 ">
       <p><span class="border-b-8 border-gradient-r-curious">Virtual Roa</span>ch Races</p>
       </a>
     </div>
     <div class="relative h-80 md:h-160">
       <div class="absolute inset-0 z-0 bg-center bg-cover" style="background-image: url('/img/games/Robodog.png')"></div>
-      <a href="#" class="absolute inset-0 z-10 flex items-center justify-center text-6xl font-semibold text-white duration-300 opacity-0 bg-cur-pink/10 hover:opacity-100 "><p>Machina Races<br><span class="border-b-8 border-gradient-r-curious">K9 Edition&nbsp;&nbsp;</span></p></a>
+      <a href="#" class="absolute inset-0 z-10 flex items-center justify-center text-6xl font-semibold text-white duration-300 opacity-0 bg-black/50 hover:opacity-100 "><p>Machina Races<br><span class="border-b-8 border-gradient-r-curious">K9 Edition&nbsp;&nbsp;</span></p></a>
     </div>
     <div class="relative h-80 md:h-160">
       <div class="absolute inset-0 z-0 bg-center bg-cover" style="background-image: url('/img/games/CyberRace.png')"></div>
-      <a href="#" class="absolute inset-0 z-10 flex items-center justify-center text-6xl font-semibold text-white duration-300 opacity-0 bg-cur-pink/10 hover:opacity-100"><p>Machina Races<br><span class="border-b-8 border-gradient-r-curious">CyberRaces</span></p></a>
+      <a href="#" class="absolute inset-0 z-10 flex items-center justify-center text-6xl font-semibold text-white duration-300 opacity-0 bg-black/50 hover:opacity-100"><p>Machina Races<br><span class="border-b-8 border-gradient-r-curious">CyberRaces</span></p></a>
     </div>
     <div class="relative h-80 md:h-160">
       <div class="absolute inset-0 z-0 bg-center bg-cover" style="background-image: url('/img/games/TronBikers.png')"></div>
-      <a href="#" class="absolute inset-0 z-10 flex items-center justify-center text-6xl font-semibold text-white duration-300 opacity-0 bg-cur-pink/10 hover:opacity-100"><p>Machina Races<br><span class="border-b-8 border-gradient-r-curious">Tron Bikers</span></p></a>
+      <a href="#" class="absolute inset-0 z-10 flex items-center justify-center text-6xl font-semibold text-white duration-300 opacity-0 bg-black/50 hover:opacity-100"><p>Machina Races<br><span class="border-b-8 border-gradient-r-curious">Tron Bikers</span></p></a>
     </div>
     <div class="relative h-80 md:h-160">
       <div class="absolute inset-0 z-0 bg-center bg-cover" style="background-image: url('/img/games/CatchACrab.png')"></div>
-      <a href="#" class="absolute inset-0 z-10 flex items-center justify-center text-6xl font-semibold text-white duration-300 opacity-0 bg-cur-pink/10 hover:opacity-100"><p><span class="border-b-8 border-gradient-r-curious">Chase A Cr</span>ab</p></a>
+      <a href="#" class="absolute inset-0 z-10 flex items-center justify-center text-6xl font-semibold text-white duration-300 opacity-0 bg-black/50 hover:opacity-100"><p><span class="border-b-8 border-gradient-r-curious">Chase A Cr</span>ab</p></a>
     </div>
   </div>
 </template>
