4ad3227132a57415d8db300cfa1d15d0889b262f
Tarik Kavaz <tarikkavaz@gmail.com>
2021-11-25 20:46:48 +0300

    subpages text size fix

--- src/views/Career.vue
+++ src/views/Career.vue
@@ -1,18 +1,18 @@
 <template>
   <div>
     <div class="container px-8 pt-40 mx-auto text-black lg:px-60">
-      <h2 class="inline-block pt-10 text-8xl">Curious about<br><span class="border-b-8 border-gradient-r-curious">our team</span></h2>
+      <h2 class="inline-block text-6xl">Curious about<br><span class="border-b-8 border-gradient-r-curious">our team</span></h2>
       
-      <div class="grid grid-cols-2 gap-12 my-20">
+      <div class="grid grid-cols-2 gap-12 my-10">
         <div>
-          <p class="mt-10 text-2xl font-thin">We are hiring and on the lookout for curious minds to join our team.</p>
-          <p class="mt-10 text-2xl font-thin">Check out our latest job posts here</p>
-          <p class="py-2 mt-10 text-2xl font-thin text-center text-white bg-blue-400 rounded-md">OPEN POSITION</p>
+          <p class="mt-5 text-lg font-thin">We are hiring and on the lookout for curious minds to join our team.</p>
+          <p class="mt-5 text-lg font-thin">Check out our latest job posts here</p>
+          <p class="py-2 mt-5 text-lg font-thin text-center text-white bg-blue-400 rounded-md">OPEN POSITION</p>
         </div>
         <div>
-          <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 1000 1000" enable-background="new 0 0 1000 1000" xml:space="preserve">
-<g><path d="M683.7,651.4c60.3-68,96.9-157.4,96.9-255.5c0-212.8-172.5-385.3-385.3-385.3C182.5,10.6,10,183.1,10,395.9c0,212.8,172.5,385.3,385.3,385.3c98.7,0,188.7-37.1,256.8-98.1l306.2,306.2l31.6-31.6L683.7,651.4z M636.1,636.7c-31.3,31.3-67.7,55.9-108.3,73c-42,17.7-86.5,26.7-132.5,26.7c-46,0-90.6-9-132.5-26.7c-40.5-17.1-77-41.7-108.3-73c-31.3-31.3-55.9-67.7-73-108.3c-17.7-42-26.7-86.5-26.7-132.5c0-46,9-90.6,26.7-132.5c17.1-40.5,41.7-77,73-108.3c31.3-31.3,67.7-55.9,108.3-73c42-17.7,86.5-26.7,132.5-26.7c46,0,90.6,9,132.5,26.7c40.5,17.1,77,41.7,108.3,73c31.3,31.3,55.9,67.7,73,108.3c17.7,42,26.7,86.5,26.7,132.5c0,46-9,90.6-26.7,132.5C692,569,667.4,605.5,636.1,636.7z"/></g>
-</svg>
+          <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 1000 1000" enable-background="new 0 0 1000 1000" xml:space="preserve" class="h-40">
+          <g><path d="M683.7,651.4c60.3-68,96.9-157.4,96.9-255.5c0-212.8-172.5-385.3-385.3-385.3C182.5,10.6,10,183.1,10,395.9c0,212.8,172.5,385.3,385.3,385.3c98.7,0,188.7-37.1,256.8-98.1l306.2,306.2l31.6-31.6L683.7,651.4z M636.1,636.7c-31.3,31.3-67.7,55.9-108.3,73c-42,17.7-86.5,26.7-132.5,26.7c-46,0-90.6-9-132.5-26.7c-40.5-17.1-77-41.7-108.3-73c-31.3-31.3-55.9-67.7-73-108.3c-17.7-42-26.7-86.5-26.7-132.5c0-46,9-90.6,26.7-132.5c17.1-40.5,41.7-77,73-108.3c31.3-31.3,67.7-55.9,108.3-73c42-17.7,86.5-26.7,132.5-26.7c46,0,90.6,9,132.5,26.7c40.5,17.1,77,41.7,108.3,73c31.3,31.3,55.9,67.7,73,108.3c17.7,42,26.7,86.5,26.7,132.5c0,46-9,90.6-26.7,132.5C692,569,667.4,605.5,636.1,636.7z"/></g>
+          </svg>
         </div>
       </div>
     </div>
--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <div class="container px-8 pt-40 mx-auto text-black lg:px-60">
-      <h2 class="inline-block pt-10 border-b-8 text-8xl border-gradient-r-curious">Contact Us</h2>
-      <p class="mt-10 text-2xl font-thin">Curious about our games? Get in <a href="#" class="text-transparent underline bg-clip-text bg-gradient-to-r from-cur-pink to-cur-blue hover:text-cur-pink">contact</a> with our team and send us a message.</p>
+      <h2 class="inline-block text-6xl border-b-8 border-gradient-r-curious">Contact Us</h2>
+      <p class="mt-5 text-lg font-thin">Curious about our games? Get in <a href="#" class="text-transparent underline bg-clip-text bg-gradient-to-r from-cur-pink to-cur-blue hover:text-cur-pink">contact</a> with our team and send us a message.</p>
     </div>
   </div>
 </template>
