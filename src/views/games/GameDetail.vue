c889132eeedb4f9807b6854e1cd5dc9336420a08
Tarik Kavaz <tarikkavaz@gmail.com>
2021-12-31 19:29:30 +0300

    back button mobile fix

--- src/views/games/GameDetail.vue
+++ src/views/games/GameDetail.vue
@@ -10,7 +10,7 @@
       <div class="grid grid-cols-6 gap-4 auto-cols-max">
         <div class="relative">
           <router-link :to="`/${$i18n.locale}/games/`" class="absolute top-0 right-0 w-16 mb-8 md:mr-6">
-            <svg class="w-10 h-10 md:w-16 md:h-16 fill-cur-blue" viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
+            <svg class="w-10 h-10 ml-4 md:w-16 md:h-16 md:ml-0 fill-cur-blue" viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
               <path d="M100 15a85 85 0 1 0 85 85 84.93 84.93 0 0 0-85-85Zm0 150a65 65 0 1 1 65-65 64.87 64.87 0 0 1-65 65Zm16.5-107.5a9.67 9.67 0 0 0-14 0L74 86a19.92 19.92 0 0 0 0 28.5l28.5 28.5a9.9 9.9 0 0 0 14-14l-28-29L117 71.5c3.5-3.5 3.5-10-.5-14Z"/>
             </svg>
           </router-link>
