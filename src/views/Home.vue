00cc5c9f8eebff83ca609afa16f9b36b335d8706
Tarik Kavaz <tarikkavaz@gmail.com>
2022-01-06 22:31:47 +0300

    video webm

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,8 +2,9 @@
 <div class="relative">
   <section class="h-screen bg-black">
     <div class="relative w-full h-screen mt-4">
-      <video autoplay="" muted="" loop="" playsinline="" preload="auto" class="absolute z-0 object-cover w-full h-full md:object-cover">
+      <video autoplay muted loop playsinline preload="auto" class="absolute z-0 object-cover w-full h-full md:object-cover">
         <source src="/mov/CockroachRacers.mp4" type="video/mp4" />
+        <source src="/mov/CockroachRacers.webm" type="video/webm" />
       </video>
       <div class="relative z-10 w-screen">
         <div class="flex items-center justify-center h-screen">
