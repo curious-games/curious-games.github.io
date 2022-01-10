f81b0cc17c1a9ffab1cbcb438f70c420ff8e8d01
Tarik Kavaz <tarikkavaz@gmail.com>
2022-01-10 14:52:56 +0300

    i18n off

--- src/main.js
+++ src/main.js
@@ -1,13 +1,14 @@
 import { createApp } from 'vue'
-import { createI18n } from 'vue-i18n'
+// import { createI18n } from 'vue-i18n'
 import VuePlyr from 'vue-plyr'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import './index.css'
 
-const i18n = createI18n({
+// const i18n = createI18n({
   
-})
+// })
 
-createApp(App).use(router).use(i18n).use(store).use(VuePlyr).mount('#app')
+// createApp(App).use(router).use(i18n).use(store).use(VuePlyr).mount('#app')
+createApp(App).use(router).use(store).use(VuePlyr).mount('#app')
