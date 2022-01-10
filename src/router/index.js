1bb8cd0429b091f006aa75693fc5a192ffa23483
Tarik Kavaz <tarikkavaz@gmail.com>
2022-01-10 10:27:07 +0300

    align

--- src/router/index.js
+++ src/router/index.js
@@ -1,73 +1,62 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
-    { path: "/", redirect: "/en/" },
-    {
-      path: '/en/',
-      name: 'Home',
-      component: () => import('/src/views/Home.vue'),
-      meta: { title: 'Curious Games - Welcome'}
-    },
-    {
-      path: '/en/games',
-      name: 'Games',
-      component: () => import('/src/views/games/Games.vue'),
-      meta: { title: 'Curious Games - Games'}
-    },
-    {
-      path: '/en/games/:slug',
-      name: 'GameDetail',
-      component: () => import('/src/views/games/GameDetail.vue'),
-      props: true,
-      meta: { title: 'Curious Games - Game Detail'}
-    },
-    {
-      path: '/en/career',
-      name: 'Career',
-      component: () => import('/src/views/Career.vue'),
-      meta: { title: 'Curious Games - Career'}
-    },
-    {
-      path: '/en/contact',
-      name: 'Contact',
-      component: () => import('/src/views/Contact.vue'),
-      meta: { title: 'Curious Games - Contact'}
-    },
-    {
-      path: '/en/partners',
-      name: 'Partners',
-      component: () => import('/src/views/Partners.vue'),
-      meta: { title: 'Curious Games - Partners'}
-    },
-    {
-      path: '/:catchAll(.*)',
-      name: 'FourOFour',
-      component: () => import('/src/views/FourOFour.vue')
-    }
+  { 
+    path: "/", 
+    redirect: "/en/" 
+  },
+  {
+    path: '/en/',
+    name: 'Home',
+    component: () => import('/src/views/Home.vue'),
+    meta: { title: 'Curious Games - Welcome'}
+  },
+  {
+    path: '/en/games',
+    name: 'Games',
+    component: () => import('/src/views/games/Games.vue'),
+    meta: { title: 'Curious Games - Games'}
+  },
+  {
+    path: '/en/games/:slug',
+    name: 'GameDetail',
+    component: () => import('/src/views/games/GameDetail.vue'),
+    props: true,
+    meta: { title: 'Curious Games - Game Detail'}
+  },
+  {
+    path: '/en/career',
+    name: 'Career',
+    component: () => import('/src/views/Career.vue'),
+    meta: { title: 'Curious Games - Career'}
+  },
+  {
+    path: '/en/contact',
+    name: 'Contact',
+    component: () => import('/src/views/Contact.vue'),
+    meta: { title: 'Curious Games - Contact'}
+  },
+  {
+    path: '/en/partners',
+    name: 'Partners',
+    component: () => import('/src/views/Partners.vue'),
+    meta: { title: 'Curious Games - Partners'}
+  },
+  {
+    path: '/:catchAll(.*)',
+    name: 'FourOFour',
+    component: () => import('/src/views/FourOFour.vue')
+  }
 ]
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes,
+  history: createWebHistory(),
+  routes,
 })
 
 router.beforeEach((to, from, next) => {
   document.title = to.meta.title ? to.meta.title : "Curious Games"
   next()
+})
 
-  // const paramsLocale = to.params.locale
-
-  // if (!SUPPORT_LOCALES.includes(paramsLocale)) {
-  //   return next(`/${locale}`)
-  // }
-
-  // if (!i18n.global.availableLocales.includes(paramsLocale)) {
-  //   await loadLocaleMessages(i18n, paramsLocale)
-  // }
-
-  // setI18nLanguage(i18n, paramsLocale)
-
-  // return next()
- })
-
- export default router
+export default router
