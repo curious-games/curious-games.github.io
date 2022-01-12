8e13dd7884c9886444ac0e4238a66ed3673c2173
Tarik Kavaz <tarikkavaz@gmail.com>
2022-01-12 12:41:14 +0300

    fix detail mobile

--- src/views/games/GameDetail.vue
+++ src/views/games/GameDetail.vue
@@ -13,24 +13,24 @@
       </template>
     </carousel>
     <div class="container relative py-10 mx-auto text-black md:px-0">
-      <div class="grid grid-cols-6 gap-4 auto-cols-max">
-        <div class="relative">
+      <div class="grid grid-cols-6 gap-4 mx-4 md:mx-0 auto-cols-max">
+        <div class="relative mb-10 md:mb-0">
           <router-link :to="`/en/games/`" class="absolute top-0 right-0 w-16 pl-2 mb-8 md:mr-6">
-            <svg class="w-10 h-10 ml-4 md:w-16 md:h-16 md:ml-0 fill-cur-blue" viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
+            <svg class="w-10 h-10 ml-2 md:ml-4 md:w-16 md:h-16 md:ml-0 fill-cur-blue" viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
               <path d="M100 15a85 85 0 1 0 85 85 84.93 84.93 0 0 0-85-85Zm0 150a65 65 0 1 1 65-65 64.87 64.87 0 0 1-65 65Zm16.5-107.5a9.67 9.67 0 0 0-14 0L74 86a19.92 19.92 0 0 0 0 28.5l28.5 28.5a9.9 9.9 0 0 0 14-14l-28-29L117 71.5c3.5-3.5 3.5-10-.5-14Z"/>
             </svg>
           </router-link>
         </div>
-        <div class="grid col-span-5 md:pl-6">
+        <div class="grid col-span-6 md:col-span-5 md:pl-6">
           <div class="col-span-5 mb-8 text-2xl font-semibold md:text-6xl">
             <p v-html="game.title"></p>
           </div>
-          <div class="col-span-5 col-start-1 px-4 md:col-span-3 md:font-thin md:pl-0 md:pr-4 ">
+          <div class="col-span-5 col-start-1 pr-4 md:px-4 md:col-span-3 md:font-thin md:pl-0 ">
             <h2 class="mb-8 text-xl font-semibold md:text-3xl">About game</h2>
             <div class="text-black about" v-html="game.about"></div>
           </div>
           <div class="col-span-6 md:col-span-2 md:text-right">
-            <h2 class="px-4 mb-8 text-xl font-semibold md:text-3xl md:px-0">Specifications</h2>
+            <h2 class="mb-8 text-xl font-semibold md:px-4 md:text-3xl md:px-0">Specifications</h2>
             <div class="grid grid-cols-2 gap-4 p-6 bg-gray-100 rounded-lg bg-opacity-70 md:font-thin">
               <div v-if="game.date" class="text-gray-800">Release Date</div><div class="text-gray-500">{{game.date}}</div>
               <div v-if="game.type" class="text-gray-800">Game type</div><div class="text-gray-500">{{game.type}}</div>
