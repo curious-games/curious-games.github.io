b7181acc257aceb1fdccd5061d6ffeb36b66884c
Tarik Kavaz <tarikkavaz@gmail.com>
2022-01-18 19:03:01 +0300

    typo Importance

--- src/views/Partners.vue
+++ src/views/Partners.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container px-8 pt-40 pb-12 mx-auto text-black mb:pb-0 lg:px-60">
     <h2 class="inline-block text-6xl border-b-8 border-gradient-r-curious">Partners</h2>
-    <p class="mt-5 text-lg md:font-thin">At Curious Games we understand the important of seamless game integration that is why we provide all our partners tailored customer support services and powerful automated development environments for testing at each stage of integration, including full API documentation.</p>
+    <p class="mt-5 text-lg md:font-thin">At Curious Games we understand the importance of seamless game integration that is why we provide all our partners tailored customer support services and powerful automated development environments for testing at each stage of integration, including full API documentation.</p>
     <p class="mt-5 text-lg md:font-thin">To learn more about integration to our Curious Games Remote Gaming Server (RGS), please contact us.</p>
     <div class="grid grid-cols-3 gap-12 my-5">
       <div class="col-span-3 md:col-span-1">
