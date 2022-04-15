cabd50a2dfb475dba17f9c102839cdaec7f758be
Tarik Kavaz <tarikkavaz@gmail.com>
2022-04-15 10:20:02 +0300

    video

--- assets/carousel.es.a3729e75.js
+++ assets/carousel.es.a3729e75.js
@@ -1,4 +1,4 @@
-import{i as se,j as i,k as L,p as w,l as pe,q as Se,s as ge,u as z,v as re,x as ye,y as _,z as b}from"./index.95ea1feb.js";/**
+import{i as se,j as i,k as L,p as w,l as pe,q as Se,s as ge,u as z,v as re,x as ye,y as _,z as b}from"./index.e33d8662.js";/**
  * Vue 3 Carousel 0.1.38
  * (c) 2022
  * @license MIT
