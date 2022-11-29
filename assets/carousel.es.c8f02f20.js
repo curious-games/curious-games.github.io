da9a9831c71318d354903ae6e78c7f5271e07821
Tarik Kavaz <tarikkavaz@gmail.com>
2022-11-29 13:37:39 +0300

    pixi demo build

--- assets/carousel.es.c8f02f20.js
+++ assets/carousel.es.c8f02f20.js
@@ -1,4 +1,4 @@
-import{i as se,j as i,k as L,p as w,l as pe,q as Se,s as ge,u as z,v as re,x as ye,y as _,z as b}from"./index.f44b6e2d.js";/**
+import{i as se,j as i,k as L,p as w,l as pe,q as Se,s as ge,u as z,v as re,x as ye,y as _,z as b}from"./index.22766d31.js";/**
  * Vue 3 Carousel 0.1.38
  * (c) 2022
  * @license MIT
