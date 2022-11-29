0bb11c7dd54525beae3129be399bc7b458694c97
Tarik Kavaz <tarikkavaz@gmail.com>
2022-11-29 13:37:01 +0300

    pixi demo run

--- src/views/HypercarMultiplier.vue
+++ src/views/HypercarMultiplier.vue
@@ -1,5 +1,6 @@
 <template>
-
+<div class="w-full h-full" style="background-color: #ee4a0d;">
+  <div id="game" style="width: 100%;height: 100%;text-align: center; display: inline-block; "></div></div>
 </template>
 
 <script>
