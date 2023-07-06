3006bab1188bdf5d8d367321144741ab42f69c38
Tarik Kavaz <tarikkavaz@gmail.com>
2023-07-06 11:56:05 +0300

    revize1deploy

--- assets/carousel.es.0eca9452.js
+++ assets/carousel.es.0eca9452.js
@@ -1,4 +1,4 @@
-import{i as se,j as i,k as L,p as w,l as pe,q as Se,s as ge,u as z,v as re,x as ye,y as _,z as b}from"./index.22766d31.js";/**
+import{i as se,j as i,k as L,p as w,l as pe,q as Se,s as ge,u as z,v as re,x as ye,y as _,z as b}from"./index.36b88331.js";/**
  * Vue 3 Carousel 0.1.38
  * (c) 2022
  * @license MIT
