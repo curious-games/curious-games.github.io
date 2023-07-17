a51585563b67d4a26def52fc788dcdbdf8f47d42
Tarik Kavaz <tarikkavaz@gmail.com>
2023-07-17 12:02:31 +0300

    agir videolar cikartildi

--- src/views/games/Games.vue
+++ src/views/games/Games.vue
@@ -2,7 +2,7 @@
   <div class="mt-16 md:mt-29">
     <div class="relative h-80 md:h-108" v-for="game in games" :key="game.id">
       <div class="absolute inset-0 z-0 bg-center bg-cover" v-bind:style="{ backgroundImage: 'url(' + game.image + ')' }"></div>
-      <router-link :to="`/en/games/` + game.slug + `/`" class="absolute inset-0 z-10 flex items-center justify-center text-4xl font-semibold text-white duration-300 md:text-6xl md:opacity-0 md:bg-black/50 hover:opacity-100 ">
+      <router-link :to="`/en/games/` + game.slug + ``" class="absolute inset-0 z-10 flex items-center justify-center text-4xl font-semibold text-white duration-300 md:text-6xl md:opacity-0 md:bg-black/50 hover:opacity-100 ">
       <div class="md:w-2/5">
         <p v-html="game.title"></p>
       </div>
