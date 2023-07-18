e6d039536c44bc965e9a5b8c4543b2548a07ffbd
Tarik Kavaz <tarikkavaz@gmail.com>
2023-07-18 10:49:21 +0300

    h1 hw buidl

--- dist/assets/carousel.es.01cb3257.js
+++ dist/assets/carousel.es.01cb3257.js
@@ -1,4 +1,4 @@
-import{i as se,j as i,k as L,p as w,l as pe,q as Se,s as ge,u as z,v as re,x as ye,y as _,z as b}from"./index.55a0d701.js";/**
+import{i as se,j as i,k as L,p as w,l as pe,q as Se,s as ge,u as z,v as re,x as ye,y as _,z as b}from"./index.8823e65a.js";/**
  * Vue 3 Carousel 0.1.38
  * (c) 2022
  * @license MIT
